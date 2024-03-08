07fbd5edaaee95df6fc669f2efb93eaba84d7fbc
MatteoRuffa <cortese_17@protonmail.com>
2024-03-08 14:32:09 +0100

    add js function

--- js/mr-script.js
+++ js/mr-script.js
@@ -19,8 +19,18 @@ fornito in consegna.
 
 /* SCRIVO PROGRAMMA CHE STAMPA IN CONSOLE I NUMERI DA 1 A 100  */
 
+
+
 for (let i= 1; i <=100; i++) {
-    console.log(i);
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
 }
 
 
